4b33532d4f36b6c2440d4e2c66c380d95b3cc1be
Dmitry Kotov <dmalexkotov@yandex.ru>
2020-08-24 22:35:32 +1000

    Описание проекта

---
+++ dfx_art/static/js/project.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.single_progect_descr').find('img').each(function(i, x){
+        const itemsCount = $(x).nextAll('p').length;
+        if (itemsCount > 0){
+            $(x).nextAll('p')[0].remove();
+        }
+    })
+});
