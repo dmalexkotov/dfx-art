7f51fbfb386d0a9bacfd0a044e0a066136149dbf
Dmitry Kotov <dmalexkotov@yandex.ru>
2020-08-24 15:00:57 +1000

    PG

--- dfx_art/settings/base.py
+++ dfx_art/settings/base.py
@@ -90,9 +90,13 @@ WSGI_APPLICATION = "dfx_art.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db', "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dfx_art',
+        'USER': 'dfx_art',
+        'PASSWORD': 'dfx_art',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
