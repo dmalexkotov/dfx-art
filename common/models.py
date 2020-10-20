28005f17e0a2b8b77ada51c9199cf7785a4a81f4
Dmitry Kotov <dmalexkotov@yandex.ru>
2020-10-21 00:59:12 +1000

    Водный знак

--- common/models.py
+++ common/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch.dispatcher import receiver
 from django.http import Http404
-from wagtail.core import blocks
+from django.utils.translation import gettext_lazy as _
 from wagtail.core.fields import StreamField, RichTextField
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import StreamFieldPanel, RichTextFieldPanel, FieldPanel
+from wagtail.admin.edit_handlers import (
+    StreamFieldPanel, RichTextFieldPanel, FieldPanel,
+)
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.images.models import Rendition
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 
 from common.blocks import ContactBlock
+
 
 @register_setting
 class SiteSettings(BaseSetting):
@@ -14,12 +21,33 @@ class SiteSettings(BaseSetting):
         ('contact', ContactBlock())
     ])
     email = models.CharField(max_length=60, help_text='Add email')
+
+    watermark_logo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text=_('Watermark logo'),
+    )
+
+    watermark_background = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text=_('Watermark backgroud'),
+    )
+
     privacy_text = RichTextField(null=True, blank=True, help_text='Privacy text')
-    
+
     panels = [
         StreamFieldPanel('contacts'),
         RichTextFieldPanel('privacy_text'),
-        FieldPanel('email')
+        FieldPanel('email'),
+        ImageChooserPanel('watermark_logo'),
+        ImageChooserPanel('watermark_background'),
     ]
 
 
@@ -38,3 +66,8 @@ class PrivacyPage(Page):
     content_panels = Page.content_panels + [
         RichTextFieldPanel('content')
     ]
+
+
+@receiver(post_save, sender=SiteSettings)
+def delete_all_image_rendition(**kwargs):
+    Rendition.objects.all().delete()
--- common/wagtail_hooks.py
+++ common/wagtail_hooks.py
@@ -1,5 +1,4 @@
 from wagtail.core import hooks
-from wagtail.images import image_operations
 
 from django.conf import settings
 from willow.plugins.pillow import PillowImage
@@ -7,42 +6,79 @@ from willow.plugins.pillow import PillowImage
 from wagtail.images.image_operations import Operation
 from PIL import Image
 
+from common.models import SiteSettings
+
 
 class WatermarkedOperation(Operation):
     # assumes an image in directory /app/static/watermark/
-    watermark_path = settings.STATIC_ROOT + '/img/WaterLogo.png'
+    watermark_logo_path = settings.STATIC_ROOT + '/img/WaterLogo.png'
+    watermark_bg_path = settings.STATIC_ROOT + '/img/Watermark.png'
 
     opacity = 0.5
 
     def run(self, willow, image, env):
         # willow is the instance of the source image loaded into willow
-        # image is the wagtail image representation which has methods like image.get_focal_point()
+        # image is the wagtail image representation which has methods like
+        # image.get_focal_point()
         # env contains additional info about the execution environment. 
         # simple transforms can be made by adding to env, such as jpeg_quality
 
-        watermark = Image.open(self.watermark_path).convert("RGBA")
-
-        wm_width, wm_height = watermark.size
         source_width, source_height = willow.get_size()
+        manipulated_image = Image.new(
+            'RGBA',
+            (source_width, source_height),
+            (255, 255, 255, 1)
+        )
 
-        wm_ratio = wm_width/wm_height
-        target_wm_width = source_width / 3.5
-        target_wm_height = target_wm_width/wm_ratio
+        manipulated_image.paste(willow.get_pillow_image(), (0, 0))
 
-        wm_offset = int(source_width * 0.03)
+        settings = SiteSettings.objects.all().first()
 
-        # watermark = watermark.resize((int(target_wm_width), int(target_wm_height)), Image.LANCZOS)
+        if settings.watermark_logo:
+            watermark_logo = Image.open(
+                settings.watermark_logo.file.path
+            ).convert("RGBA")
 
-        offset_x = (source_width - watermark.size[0]) - wm_offset
-        offset_y = (source_height - watermark.size[1]) - wm_offset
+            wm_width, wm_height = watermark_logo.size
 
-        manipulated_image = Image.new('RGBA', (source_width, source_height), (255, 255, 255, 1))
-        manipulated_image.paste(willow.get_pillow_image(), (0,0))
-        manipulated_image.paste(watermark, (offset_x, offset_y), watermark)
+            # wm_ratio = wm_width/wm_height
+            # target_wm_width = source_width / 3.5
+            # target_wm_height = target_wm_width/wm_ratio
+
+            wm_offset = int(source_width * 0.01)
+
+            watermark_logo = watermark_logo.resize(
+                (int(wm_width), int(wm_height)),
+                Image.LANCZOS
+            )
+
+            offset_x = (source_width - watermark_logo.size[0]) - wm_offset
+            offset_y = (source_height - watermark_logo.size[1]) - wm_offset
+
+            manipulated_image.paste(
+                watermark_logo, (offset_x, offset_y), watermark_logo
+            )
+
+        if settings.watermark_background:
+            watermark_bg = Image.open(
+                settings.watermark_background.file.path
+            ).convert("RGBA")
+            wm_bg_width, wm_bg_height = watermark_bg.size
+
+            x_repeat = int(source_width / wm_bg_width) + 1
+            y_repeat = int(source_height / wm_bg_height) + 1
+
+            for x_index in range(x_repeat):
+                for y_index in range(y_repeat):
+                    manipulated_image.paste(
+                        watermark_bg,
+                        (x_index * wm_bg_width, y_index * wm_bg_height),
+                        watermark_bg
+                    )
 
         return PillowImage(manipulated_image)
 
-    def construct(self):
+    def construct(self, *args, **kwargs):
         pass
 
 
